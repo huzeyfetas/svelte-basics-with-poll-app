64bd855fcd35cf329611dafa9fb4c779d7a26efd
Hüzeyfe TAŞ <huzeyfetas@outlook.com>
2022-03-01 19:27:29 +0300

    header and footer added

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+  <p class="footer-copyright">@Copyright 2022 Poll Ninja</p>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+  .footer-copyright {
+    color: #ccc;
+    text-align: center;
+
+    display: inline-block;
+    padding: 12px 20px 0 20px;
+    border-top: 1px solid #ccc;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<header>
+  <img src="/img/poll_ninja_logo.svg" alt="Poll Ninja Logo" />
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    max-width: 180px;
+  }
+</style>
